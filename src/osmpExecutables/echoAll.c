6f1a82bb8b9975514e9d85806c901ce3b249f51a
Konrad Alan Skwarski <ks413864@fh-muenster.de>
2025-04-16 11:57:41 +0200

    Build angepasst

--- src/osmpExecutables/echoAll.c
+++ src/osmpExecutables/echoAll.c
@@ -3,21 +3,49 @@
 //
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include "../osmpLibrary/OSMP.h"
 
-/*
- * echoAll.c
- * Echoes all command line arguments
- * and prints the process ID and parent process ID.
- */
 int main(int argc, char *argv[]) {
+    int rv;
+    int size, rank;
 
-    fprintf(stdout,"Programmname: %s\n", argv[0]);
-
-    for (int i = 1; i < argc ; i++) {
-        fprintf(stdout,"Argument %d: %s\n", i, argv[i]);
-        //sleep(1);
+    // Initialisiere OSMP
+    rv = OSMP_Init(&argc, &argv);
+    if (rv != OSMP_SUCCESS) {
+        fprintf(stderr, "OSMP_Init failed\n");
+        return EXIT_FAILURE;
     }
 
-    fprintf(stdout,"PID: %d \nPPID: %d \n", getpid(), getppid());
-    return 0;
+    // Bestimme Gesamtanzahl der Prozesse
+    rv = OSMP_Size(&size);
+    if (rv != OSMP_SUCCESS) {
+        fprintf(stderr, "OSMP_Size failed\n");
+        return EXIT_FAILURE;
+    }
+
+    // Bestimme eigenen Rank
+    rv = OSMP_Rank(&rank);
+    if (rv != OSMP_SUCCESS) {
+        fprintf(stderr, "OSMP_Rank failed\n");
+        return EXIT_FAILURE;
+    }
+
+    // Ausgabe wie zuvor
+    fprintf(stdout, "Programmname: %s\n", argv[0]);
+    for (int i = 1; i < argc ; i++) {
+        fprintf(stdout, "Argument %d: %s\n", i, argv[i]);
+    }
+
+    fprintf(stdout, "PID: %d | PPID: %d\n", getpid(), getppid());
+    fprintf(stdout, "Hallo von Rank %d von %d\n", rank, size);
+
+    // Finalize
+    rv = OSMP_Finalize();
+    if (rv != OSMP_SUCCESS) {
+        fprintf(stderr, "OSMP_Finalize failed\n");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
